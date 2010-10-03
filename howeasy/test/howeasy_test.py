b13e40e1ca5fbdfee63bac06a019f90b9db3ad6b
Tiago <tiago@MacBook-Pro.local>
2010-10-03 21:09:55 +0200

    1 tab -> 2 spaces.

--- howeasy/test/howeasy_test.py
+++ howeasy/test/howeasy_test.py
@@ -3,11 +3,11 @@ import unittest
 from howeasy.howeasy import HowEasy
 
 class HowEasyTest(unittest.TestCase):
-	
-	def test_point_val(self):
-		howeasy = HowEasy()
-		assert howeasy.point_val('This is a problem statement') == 500
-		assert howeasy.point_val('523hi.') == 250
-		assert howeasy.point_val('Implement a class H5 which contains some method.') == 500
-		assert howeasy.point_val('Searching') == 1000
-		assert howeasy.point_val(' no9 . wor7ds he8re. hj..') == 250
+  
+  def test_point_val(self):
+    howeasy = HowEasy()
+    assert howeasy.point_val('This is a problem statement') == 500
+    assert howeasy.point_val('523hi.') == 250
+    assert howeasy.point_val('Implement a class H5 which contains some method.') == 500
+    assert howeasy.point_val('Searching') == 1000
+    assert howeasy.point_val(' no9 . wor7ds he8re. hj..') == 250
