24462ce591b60ccadfa36d64ec7f258ea60c9b1e
Tiago <tiago@tiago-fernandezs-macbook-pro.local>
2010-10-13 01:26:59 +0200

    Added suggestions generation feature.

--- predtext/src/PredText.groovy
+++ predtext/src/PredText.groovy
@@ -2,35 +2,51 @@ class PredText {
   
   static final def dictionary =
     [
-      2 : ['a', 'b', 'c'],
-      3 : ['d', 'e', 'f'],
-      4 : ['g', 'h', 'i'],
-      5 : ['j', 'k', 'l'],
-      6 : ['m', 'n', 'o'],
-      7 : ['p', 'q', 'r', 's'],
-      8 : ['t', 'u', 'v'],
-      9 : ['w', 'x', 'y', 'z']
+      '2' : ['a', 'b', 'c'],
+      '3' : ['d', 'e', 'f'],
+      '4' : ['g', 'h', 'i'],
+      '5' : ['j', 'k', 'l'],
+      '6' : ['m', 'n', 'o'],
+      '7' : ['p', 'q', 'r', 's'],
+      '8' : ['t', 'u', 'v'],
+      '9' : ['w', 'x', 'y', 'z']
     ]
   
-  // TODO Generate the vocabulary automatically.
-  static final def vocabulary = [
-    '7253'  : ['pale', 'sake', 'sale', 'rake'],
-    '43556' : ['hello'],
-  ]
-  
   String getDigits(String word) {
-    def digits = []
+    def digits = ''
     for (letter in word) {
       dictionary.each { digit, letters ->
         if (letters.contains(letter))
-          digits << digit
+          digits += digit
       }
     }
-    return digits.join('')
+    return digits
   }
   
-  List getSuggestions(String word) {
-    def digits = getDigits(word)
-    return vocabulary[digits]
+  List getSuggestions(String digits) {
+    def (letters, startIndexes, endIndexes) = initSuggestions(digits)
+    def suggestions = [], formatter = new java.text.DecimalFormat(startIndexes)
+    0.upto(endIndexes.toInteger()) {
+      def word = '', indexes = formatter.format("$it".toInteger())
+      for (int i = 0; i < indexes.length(); i++) {
+        def index = "${indexes[i]}".toInteger()
+        def letter = letters[i][index]
+        if (letter != null) word += letter
+      }
+      if (word.length() == indexes.length())
+        suggestions << word
+    }
+    return suggestions
+  }
+
+  private def initSuggestions(digits) {
+    def letters = [], startIndexes = '', endIndexes = ''
+    for (digit in digits) {
+      def options = dictionary[digit]
+      letters << options
+      startIndexes += 0
+      endIndexes += options.size() - 1
+    }
+    [letters, startIndexes, endIndexes]
   }
 }
--- predtext/test/PredTextTest.groovy
+++ predtext/test/PredTextTest.groovy
@@ -4,13 +4,19 @@ class PredTextTest extends GroovyTestCase {
   
   void testGetDigits() {
     assert predText.getDigits('hello') == '43556'
+    assert predText.getDigits('sale') == '7253'
   }
   
   void testGetSuggestions() {
-    def suggestions = predText.getSuggestions('rake')
-    assert suggestions.contains('pale')
-    assert suggestions.contains('sake')
+    def suggestions = predText.getSuggestions('43556')
+    assert suggestions.contains('hello')
+    assert suggestions.contains('geklo')
+    assert suggestions.contains('gekko')
+    
+    suggestions = predText.getSuggestions('7253')
     assert suggestions.contains('sale')
     assert suggestions.contains('rake')
+    assert suggestions.contains('pale')
+    assert suggestions.contains('ralf')
   }
 }
